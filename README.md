f4fa2f6f623accbfbde0c21d24ca1b194da7a965
0xSardius <0xSardius@gmail.com>
2026-08-24 17:56:43 -0400

    basic skeleton of the game

---
+++ README.md
@@ -0,0 +1,3 @@
+# Turbin3 Q3 Builder Rust Guessing Game
+
+This repo is for the Turbin3 Q3 Builder program's week 1 rust guessing game, adapted from the rust book, located here: https://rust-book.cs.brown.edu/ch02-00-guessing-game-tutorial.html
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
+use std::io;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Please guess the number!");
+
+    println!("Please enter your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed {guess}");
+
 }
